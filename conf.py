9278e2a09924079b5551dde6d8664928ba3a0e8d
Likhith Manjunatha <likhithm95@gmail.com>
2020-01-29 16:27:42 +0530

    Nikola auto commit.
    Nikola version: 8.0.3

--- conf.py
+++ conf.py
@@ -152,7 +152,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3-gradients-jinja"
+THEME = "bootstrap3"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
